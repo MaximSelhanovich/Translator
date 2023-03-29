5652500d12453f666baa013f1f7bc9a84c34bad8
MaximSelhanovich <75281453+MaximSelhanovich@users.noreply.github.com>
2023-03-29 17:09:52 +0300

    Creating dicts

--- Translator/main.py
+++ Translator/main.py
@@ -1,7 +1,47 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+KEY_WORDS = {
+    'void',
+    'string',
+    'int',
+    'double',
+    'bool',
+    'true',
+    'false',
+    'if',
+    'else',
+    'while',
+    'do',
+    'for',
+    'continue',
+    'break',
+    'return'
+}
+
+KEY_SYMBOLS = {
+    '{',
+    '}',
+    '[',
+    ']',
+    '(',
+    ')',
+    ';',
+    ':'
+    ',',
+    '+',
+    '-',
+    '*',
+    '/',
+    '=',
+    '+=',
+    '-=',
+    '*=',
+    '/=',
+    '==',
+    '|',
+    '&',
+    '||',
+    '&&'
+}
 
 
 def print_hi(name):
@@ -12,5 +52,3 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
