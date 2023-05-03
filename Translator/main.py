e68e5ff060ecf57dd54e4c61d141107d554252fc
MaximSelhanovich <mselhanovich@gmail.com>
2023-05-03 13:49:19 +0300

    Identifier and keywords parsing

--- Translator/main.py
+++ Translator/main.py
@@ -19,16 +19,26 @@ class Token:
     value: Any
 
 
-class UnknownTokenError(Exception):
-    def __init__(self, line, column, token_value):
+class LexicalError(Exception):
+    def __init__(self, line, column, message):
         self.line = line
         self.column = column
-        self.token_value = token_value
-        message = f'Unknown token at ({line}, {column}) with value ({token_value})'
         self.message = message
         super().__init__(self.message)
 
 
+class UnknownTokenError(LexicalError):
+    def __init__(self, line, column, token_value):
+        self.token_value = token_value
+        self.message = f'Unknown token at ({line}, {column}) with value ({token_value})'
+        super().__init__(line, column, self.message)
+
+
+class DoubleDeclarationError(LexicalError):
+    def __init__(self, line, column, token_value):
+        self.token_value = token_value
+        self.message = f'Double declaration at ({line}, {column}) with value ({token_value})'
+        super().__init__(line, column, self.message)
 TOKENS = []
 
 
@@ -52,6 +62,7 @@ KEYWORDS = {
     'continue',
     'break',
     'return',
+    'exit',
     'cin',
     'cout'
 }
@@ -108,6 +119,7 @@ class LiteralHelper:
 class IdentifierHelper(LiteralHelper):
     value: Any
 
+
 LITERALS = []
 COMMENTS = {}
 IDENTIFIERS = []
@@ -137,16 +149,28 @@ class LexicalAnalyser:
         self.cur_line = None
         self.cur_token_id: int = 0
 
+    def check_identifier_existence(self):
+        for i in IDENTIFIERS:
+            if TOKENS[i.id].value == self.cur_token.value:
+                return True
+        return False
+
     def add_token(self):
         self.cur_token.id = self.cur_token_id
         TOKENS.append(self.cur_token)
         if self.cur_token.type == TokenType.LITERAL:
             LITERALS.append(LiteralHelper(self.cur_token.id, self.CURRENT_STATE))
-        # elif self.cur_token.type == TokenType.IDENTIFIER:
-          #  IDENTIFIERS.append()
-        print(f"id: {self.cur_token.id}, type: {self.cur_token.type}, value: {self.cur_token.value}")
+        elif self.cur_token.type == TokenType.IDENTIFIER:
+            if STATES.DEFINITION in self.CURRENT_STATE:
+                if self.check_identifier_existence():
+                    raise DoubleDeclarationError(self.cur_line_index, self.cur_col_index, self.cur_token.value)
+                IDENTIFIERS.append(IdentifierHelper(self.cur_token.id, None, None))
+                self.CURRENT_STATE ^= STATES.DEFINITION
+            elif not self.check_identifier_existence():
+                raise UnknownTokenError(self.cur_line_index, self.cur_col_index, self.cur_token.value)
+        # print(f"id: {self.cur_token.id}, type: {self.cur_token.type}, value: {self.cur_token.value}")
         self.cur_token = Token(None, None, None)
-        self.CURRENT_STATE = STATES.NOTHING
+        self.CURRENT_STATE |= STATES.NOTHING
         self.cur_token_id += 1
 
     def parse_string_literal(self):
@@ -206,8 +230,7 @@ class LexicalAnalyser:
 
     def parse_non_literal(self):
         # self.CURRENT_STATE = STATES.STRING
-        # self.cur_token.type = TokenType.LITERAL
-
+        self.cur_token.type = TokenType.IDENTIFIER
         self.cur_token.value = self.cur_line[self.cur_col_index]
         self.cur_col_index += 1
         while self.cur_col_index < len(self.cur_line):
@@ -216,11 +239,10 @@ class LexicalAnalyser:
                 if self.cur_token.value == 'true' or self.cur_token.value == 'false':
                     self.CURRENT_STATE = STATES.BOOL
                     self.cur_token.type = TokenType.LITERAL
-                elif self.cur_token.value in KEYWORDS_DEFINITION or self.cur_token.value in KEYWORDS:
-                    self.CURRENT_STATE = STATES.DEFINITION
+                elif self.cur_token.value in KEYWORDS_DEFINITION.union(KEYWORDS):
+                    if self.cur_token.value in KEYWORDS_DEFINITION:
+                        self.CURRENT_STATE = STATES.DEFINITION
                     self.cur_token.type = TokenType.KEYWORD
-                else:
-                    self.cur_token.type = TokenType.IDENTIFIER
                 self.add_token()
                 self.cur_col_index -= 1
                 return
@@ -255,5 +277,4 @@ class LexicalAnalyser:
 
 
 if __name__ == '__main__':
-    print(STATES.BOOL)
     LexicalAnalyser('test.cpp').parse()
